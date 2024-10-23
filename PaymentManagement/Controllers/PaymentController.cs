283de4c2dca1a62e5b3140069eb4f0c12955162d
minhnhat0809 <nhat892003@gmail.com>
2024-10-23 12:27:22 +0700

    add payment

---
+++ PaymentManagement/Controllers/PaymentController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+    }
+}
