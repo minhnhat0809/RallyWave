d70b3e3b8ecfa895de40e6630d382d8882108bdd
minhnhat0809 <nhat892003@gmail.com>
2024-11-03 19:23:01 +0700

    change connection string

--- ChattingManagement/Program.cs
+++ ChattingManagement/Program.cs
@@ -13,11 +13,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-//dbcontext
+//retrieve connection string from AWS Secrets Manager
+var getSecret = new GetSecret();
+var connectionString = await getSecret.GetConnectionString();
+
+//db context
 builder.Services.AddDbContext<RallyWaveContext>(options =>
 {
-    options.UseMySql(builder.Configuration.GetConnectionString("RallyWave"),
-        new MySqlServerVersion(new Version(8, 0, 39))); 
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 39))); 
 });
 
 //service
